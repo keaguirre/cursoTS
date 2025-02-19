fa277ba82bbd33ced202171df16a0708885b608b
keaguirre <ke.aguirre7@gmail.com>
2025-02-19 06:12:39 -0300

    added POO

--- src/indexPOO.ts
+++ src/indexPOO.ts
@@ -1,27 +1,53 @@
 class Personaje {
-    id: number;
-    name: string;
-    nivel: number;
-    hp: number;
-    constructor(id: number, name:string, nivel:number, hp:number) {
-        this.id = id;
-        this.name = name;
-        this.nivel = nivel;
-        this.hp = hp;
+    profesion?: string;
+    constructor(
+        public readonly id: number,
+        public name:string,
+        public nivel:number,
+        private _hp:number) {
     }
     subirNivel(): number {
         this.nivel = this.nivel + 1;
         return this.nivel;
     }
     cambiarHP(cantidad: number): number {
-        this.hp = this.hp + cantidad;
-        return this.hp;
+        this._hp = this._hp + cantidad;
+        // no pasarse del maximo
+        return this._hp;
     }
 }
 
 const personaje = new Personaje(1, 'kevin', 1, 100);
 personaje.cambiarHP(-10);
 console.log(personaje);
+
+
 if (personaje instanceof Personaje) {
     console.log('Es un personaje o no, retorna boolean');
 }
+
+//tenemos dos opciones de definicion de clases
+/*
+class Personaje {
+    readonly id: number;
+    name: string;
+    nivel: number;
+    private hp: number;
+    profesion?: string;
+    constructor(id: number, name:string, nivel:number, hp:number) {
+        this.id = id;
+        this.name = name;
+        this.nivel = nivel;
+        this.hp = hp;
+    }
+}
+    o
+class Personaje {
+    constructor(
+        public readonly id: number,
+        public name:string,
+        public nivel:number,
+        private hp:number) {
+    }
+}
+*/
